b1551339eb022315b4186e3c1e69bdf700522a26
SivaRaja <siva.raja@azurecomputer.com>
2018-05-08 17:58:42 +0700

    First commit

---
+++ spring-redis-javaconfig/src/main/java/com/concretepage/config/RedisConfig.java
@@ -0,0 +1,46 @@
+package com.concretepage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import com.concretepage.bean.Person;
+
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+@ComponentScan("com.concretepage")
+public class RedisConfig {
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		JedisPoolConfig poolConfig = new JedisPoolConfig();
+		poolConfig.setMaxTotal(5);
+		poolConfig.setTestOnBorrow(true);
+		poolConfig.setTestOnReturn(true);
+		
+		JedisConnectionFactory connectionFactory = new JedisConnectionFactory(poolConfig);
+		connectionFactory.setUsePool(true);
+		connectionFactory.setHostName("35.234.10.254");
+		connectionFactory.setPort(6379);
+		connectionFactory.setPassword("welcome");
+		
+		return connectionFactory;
+	}
+	@Bean
+	public RedisTemplate<String, Person> redisTemplate() {
+		RedisTemplate<String, Person> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setEnableTransactionSupport(true);
+		return redisTemplate;
+	}
+	@Bean
+	public StringRedisTemplate stringRedisTemplate() {
+		StringRedisTemplate stringRedisTemplate = new StringRedisTemplate(redisConnectionFactory());
+		stringRedisTemplate.setEnableTransactionSupport(true);
+		return stringRedisTemplate;
+	}	
+}
